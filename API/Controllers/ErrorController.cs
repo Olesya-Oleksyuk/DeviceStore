26963741fb2a928523c3d9b76f1084ff93726483
Lesya <olesyaoleksyuk@mail.ru>
2021-04-10 15:30:57 +0300

    Handling a Not found Endpoint Error

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  // override the routes that we get from our base API controller
+  //   redirected to the ApiResponse.cs ???
+  [Route("errors/{code}")]
+  public class ErrorController : BaseApiController
+  {
+    /// <summary>
+    ///  Add a custom action method to generate and return an ApiResponse object. 
+    /// </summary>
+    public IActionResult Error(int code)
+    {
+      return new ObjectResult(new ApiResponse(code));
+    }
+  }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -42,12 +42,17 @@ namespace API
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+
+      // Exception Handling option #1: Error Handling applying to whether or not we're in development.
+      // Use the Developer Exception Page. 
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
         // app.UseSwagger();
         // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
       }
+
+      app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
       app.UseHttpsRedirection();
 
